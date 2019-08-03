6a524b6088251329ea6925ad8013fdb9f7d22378
ぶーと / Yoshiaki Ueda <oh@bootjp.me>
2019-08-03 12:30:18 +0900

    refactoring

--- options.js
+++ options.js
@@ -1,13 +1,13 @@
 let options = {
   worldID: "",
   instanceID: "",
-  nonce:  "",
+  nonce: "",
   userUID: "",
   instanceOwnerID: "",
   instancePermission: "",
 };
 
-chrome.storage.local.get(options, (item) =>  {
+chrome.storage.local.get(options, (item) => {
   // load setting.
   for (let optionsKey in options) {
     let elm = document.getElementsByName(optionsKey);
@@ -30,23 +30,32 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }
 
-      chrome.storage.local.set(options, () => {
-        console.log("stored");
-        console.log(options);
-      });
-    };
-    const saveButton = document.getElementById('save');
-    saveButton.addEventListener('click', save);
-    const lunchButton = document.getElementById('lunch');
-    lunchButton.addEventListener('click', () => {
-     for (let optionsKey in options) {
-        let elm = document.getElementsByName(optionsKey);
-        if (optionsKey === 'instancePermission' && elm.tagName === "select") {
-          elm = elm.getElementsByTagName('option')[elm.selectedIndex];
-        }
-       options[optionsKey] = elm[0].value
+    chrome.storage.local.set(options, () => {
+      console.log("stored");
+      console.log(options);
+    });
+  };
+  const saveButton = document.getElementById('save');
+  saveButton.addEventListener('click', save);
+  const lunchButton = document.getElementById('lunch');
+  lunchButton.addEventListener('click', () => {
+    for (let optionsKey in options) {
+      let elm = document.getElementsByName(optionsKey);
+      if (optionsKey === 'instancePermission' && elm.tagName === "select") {
+        elm = elm.getElementsByTagName('option')[elm.selectedIndex];
       }
-      chrome.tabs.create({active: true, url: `vrchat://launch/?ref=vrchat.com&id=${options['world_id']}:${options['instanceID']}~private(${options['instanceOwnerID']})~nonce(${options['nonce']})~canRequestInvite`})
+      options[optionsKey] = elm[0].value
+    }
+    let params = {
+      ref: "vrchat.com", // なくてもいいんだけど，対策されても良いようにつけておく
+      id: `${options['worldID']}:${options['instanceID']}~private(${options['instanceOwnerID']})~nonce(${options['nonce']})~canRequestInvite`
+  };
+  chrome.tabs.create(
+      {
+        active: true,
+        url: 'vrchat://launch/?' + Object.keys(params).map(k => k + '=' + params[k]).join('&')
+      }
+    )
   });
 });
 
